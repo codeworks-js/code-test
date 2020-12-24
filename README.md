03dc0e7feac6213d41792002df57626dc20a72c7
Arnaud <contact@neolame.com>
2020-12-24 01:50:50 +0100

    Add Material-UI

--- README.md
+++ README.md
@@ -6,9 +6,29 @@ Mais également de disposer d'une application simple nous permettant de mettre e
 
 Et pas seulement, nous allons également tester une méthodologie de développement piloteé par les composants [CDD](https://www.learnstorybook.com/intro-to-storybook/react/en/get-started/) en utilisant les storybooks associés à Jest.
 
+Ce projet utilise [Material-UI](https://material-ui.com/fr/) comme framework de style. Ce framework utilise en interne CSS-in-js et les styled Components. Il propose une [api de Hook](https://material-ui.com/fr/styles/basics/#hook-api) ainsi qu'une [Api styled components](https://material-ui.com/fr/styles/basics/#styled-components-api).
 
 ## Contribuer
 
+Les contributions sont libre mais doivent être proposées par PullRequests
+
 ## Install
+
+```
+# intallation des dépendences
+yarn
+# lancement en mode dev
+yarn start
+# Build pour prod
+yarn build
+# Lancer les tests
+yarn test
+# Génération des fichiers de configuration utilisés par la CLI de react-script
+yarn eject
+# Lancer les storybooks
+yarn storybook
+# Builder une version statique, pour documentation par exemple
+yarn build-storybook
+``` 
 
 ...
--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
+import React from 'react';
+import CssBaseline from '@material-ui/core/CssBaseline';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <React.Fragment>
+        <CssBaseline />
+        {/* The rest of your application */}
+
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+              Edit <code>src/App.js</code> and save to reload.
+            </p>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
+          </header>
+        </div>
+      </React.Fragment>
   );
 }
 
